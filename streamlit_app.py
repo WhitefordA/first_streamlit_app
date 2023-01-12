1e74b55511e5fc17aa93ff22c48dfe084c852f77
WhitefordA <122058612+WhitefordA@users.noreply.github.com>
2023-01-12 12:59:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocekt Smoothie')
+                
+              
